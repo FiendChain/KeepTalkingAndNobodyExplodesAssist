b48b5a6f5dd98b09e1a110c9125d814c13f92b71
FiendChain <formulating.response@gmail.com>
2018-07-15 02:22:27 +1000

    Added to github pages

--- src/app/bomb/module-selector/module-selector.component.ts
+++ src/app/bomb/module-selector/module-selector.component.ts
@@ -35,7 +35,7 @@ export class BombModuleSelector implements OnInit {
         return bombModuleNames;
     }
 
-    private loadModule(): void {
+    public loadModule(): void {
         if(!this.moduleName) {
             this.module.viewContainerRef.clear();
         } else if(bombModules.has(this.moduleName)) {
