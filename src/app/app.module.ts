3414d4bedb64324fb4f64aad9f907752d90b1a99
FiendChain <formulating.response@gmail.com>
2018-07-15 23:34:38 +1000

    Improved header to webpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { BombModule } from './bomb/bomb.module';
@@ -11,6 +12,7 @@ import { BombModule } from './bomb/bomb.module';
     imports: [
         BrowserModule,
         BombModule,
+        NgbModule.forRoot(),
     ],
     providers: [],
     bootstrap: [AppComponent]
