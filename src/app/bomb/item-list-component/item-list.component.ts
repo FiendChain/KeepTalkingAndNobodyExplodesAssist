d73f94f7b629567a7af2a5abd6a52d2f0745bc52
FiendChain <formulating.response@gmail.com>
2018-07-18 13:38:40 +1000

    Added features to item list component

--- src/app/bomb/item-list-component/item-list.component.ts
+++ src/app/bomb/item-list-component/item-list.component.ts
@@ -8,19 +8,35 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     ],
 })
 export class ItemListComponent {
-    @Input('items') items: string[] = [];
-    @Input('options') options: string[] = [];
+    @Input('items') items: any[] = [];
+    @Input('options') options: any[];
     @Input('label') label: string;
     @Input('text') text: string;
-    @Output('onItem') onClick = new EventEmitter<number>();
-    @Output('onOption') onOption = new EventEmitter<string>();
+    @Input('showClose') showClose: boolean = true;
+    @Input('formatting') formatting: ClassFormatting = {};
+    
+    @Output('onItem') onClick = new EventEmitter<any>();
+    @Output('onOption') onOption = new EventEmitter<any>();
 
     // propagate click
     public itemClick(index: number): void {
         this.onClick.emit(index);
     }
 
-    public optionClick(item: string): void {
+    public optionClick(item: any): void {
         this.onOption.emit(item);
     }
+
+    // get class of an item from formatting
+    public getClass(item: string | number): string {
+        if(!this.formatting) return "";
+        var classFormat: string = this.formatting[item];
+        if(!classFormat) return "";
+        return classFormat;
+    }
+}
+
+export interface ClassFormatting {
+    [item: string]: string;
+    [item: number]: string;
 }
--- src/app/bomb/modules/memory/memory.component.ts
+++ src/app/bomb/modules/memory/memory.component.ts
@@ -1,17 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BombModuleInterface } from '../../bomb-module.interface';
+import { ClassFormatting } from '../../item-list-component/item-list.component';
+
+const buttonArrangement: number[] = [1,3,4,2];
+const buttonFormatting: ClassFormatting = {
+    1: 'background-blue',
+    2: 'background-green',
+    3: 'background-red',
+    4: 'background-yellow',
+};
+
+function getButton(position: number): number {
+    if(position <= 0 || position > buttonArrangement.length) {
+        throw "Out of index error for buttonArrangement";
+    } else {
+        return buttonArrangement[position-1];
+    }
+}
 
 @Component({
-  selector: 'app-memory',
-  templateUrl: './memory.component.html',
-  styleUrls: ['./memory.component.css']
+    selector: 'app-memory',
+    templateUrl: './memory.component.html',
+    styleUrls: ['./memory.component.css']
 })
-export class MemoryComponent implements OnInit, BombModuleInterface {
-  public name: string = 'memory';
+export class MemoryComponent implements BombModuleInterface {
+    public name: string = 'memory';
+    public inputs: Array<number> = [];
+    public outputs: Array<number> = [];
+    public maxStages: number = 5;
+    public buttonFormatting = buttonFormatting;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    public addInput(input: number): void {
+        if(this.inputs.length < 5) {
+            this.inputs.push(input);
+            let output = this.calculateOutput(input, this.inputs.length);
+            this.outputs.push(output);
+        }
+    }
 
+    public removeIndex(index: number): void {
+        this.inputs.splice(index, 1);
+        this.recomputeOutput();
+    }
+
+    // if input list was changed, recompute new output sequence
+    private recomputeOutput(): void {
+        this.outputs = [];
+        this.inputs.forEach((input, index) => {
+            let output = this.calculateOutput(input, index+1);
+            this.outputs.push(output);
+        });
+    }
+
+    public calculateOutput(input: number, stage: number): number {
+        // validate input
+        if(input <= 0 || input > 4) {
+            throw "Invalid input for memory module";
+        }
+        switch(stage) {
+        // stage 1
+        case 1:
+            if(input == 1) return getButton(2);
+            if(input == 2) return getButton(2);
+            if(input == 3) return getButton(3);
+            if(input == 4) return getButton(4);
+            break;
+        // stage 2
+        case 2:
+            if(input == 1) return 4;
+            if(input == 2) return this.getStage(1);
+            if(input == 3) return getButton(1);
+            if(input == 4) return this.getStage(1);
+            break;
+        // stage 3
+        case 3:
+            if(input == 1) return this.getStage(2);
+            if(input == 2) return this.getStage(1);
+            if(input == 3) return getButton(3);
+            if(input == 4) return 4;
+            break; 
+        // stage 4
+        case 4:
+            if(input == 1) return this.getStage(1);
+            if(input == 2) return getButton(1);
+            if(input == 3) return this.getStage(2);
+            if(input == 4) return this.getStage(2);
+            break;
+        case 5:
+            if(input == 1) return this.getStage(1);
+            if(input == 2) return this.getStage(2);
+            if(input == 3) return this.getStage(4);
+            if(input == 4) return this.getStage(3);
+            break;
+        default:
+            throw `There is no stage ${stage} for memory game`;
+        }
+    }
+
+    private getStage(stage: number): number {
+        if(this.outputs.length+1 < stage) {
+            throw "Cannot access stage ahead of output";
+        }
+        return this.outputs[stage-1];
+    }
+
+    public getClass(input: number): string {
+        return buttonFormatting[input];
+    }
 }
