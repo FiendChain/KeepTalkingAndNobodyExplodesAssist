47e2c1895bcaad5fea44dd3887b02a9661ce43f9
FiendChain <formulating.response@gmail.com>
2018-07-18 14:35:16 +1000

    Added reorganisation support to memory module

--- src/app/bomb/modules/memory/memory.component.ts
+++ src/app/bomb/modules/memory/memory.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BombModuleInterface } from '../../bomb-module.interface';
 import { ClassFormatting } from '../../item-list-component/item-list.component';
 
-const buttonArrangement: number[] = [1,3,4,2];
 const buttonFormatting: ClassFormatting = {
     1: 'background-blue',
     2: 'background-green',
@@ -10,13 +9,12 @@ const buttonFormatting: ClassFormatting = {
     4: 'background-yellow',
 };
 
-function getButton(position: number): number {
-    if(position <= 0 || position > buttonArrangement.length) {
-        throw "Out of index error for buttonArrangement";
-    } else {
-        return buttonArrangement[position-1];
-    }
-}
+const buttonDropdownFormatting: ClassFormatting = {
+    1: 'btn-primary',
+    2: 'btn-success',
+    3: 'btn-danger',
+    4: 'btn-warning',
+};
 
 @Component({
     selector: 'app-memory',
@@ -25,6 +23,7 @@ function getButton(position: number): number {
 })
 export class MemoryComponent implements BombModuleInterface {
     public name: string = 'memory';
+    public buttons: Array<number> = [1,2,3,4];
     public inputs: Array<number> = [];
     public outputs: Array<number> = [];
     public maxStages: number = 5;
@@ -45,6 +44,19 @@ export class MemoryComponent implements BombModuleInterface {
         this.recomputeOutput();
     }
 
+    public setButton(index: number, newValue: number): void {
+        let oldValue: number = this.buttons[index];
+        // swap values
+        for(var i = 0; i < this.buttons.length; i++) {
+            if(this.buttons[i] == newValue) {
+                this.buttons[i] = oldValue;
+                break;
+            }
+        }
+        this.buttons[index] = newValue;
+        this.recomputeOutput();
+    }
+
     // if input list was changed, recompute new output sequence
     private recomputeOutput(): void {
         this.outputs = [];
@@ -62,29 +74,29 @@ export class MemoryComponent implements BombModuleInterface {
         switch(stage) {
         // stage 1
         case 1:
-            if(input == 1) return getButton(2);
-            if(input == 2) return getButton(2);
-            if(input == 3) return getButton(3);
-            if(input == 4) return getButton(4);
+            if(input == 1) return this.getButton(2);
+            if(input == 2) return this.getButton(2);
+            if(input == 3) return this.getButton(3);
+            if(input == 4) return this.getButton(4);
             break;
         // stage 2
         case 2:
             if(input == 1) return 4;
             if(input == 2) return this.getStage(1);
-            if(input == 3) return getButton(1);
+            if(input == 3) return this.getButton(1);
             if(input == 4) return this.getStage(1);
             break;
         // stage 3
         case 3:
             if(input == 1) return this.getStage(2);
             if(input == 2) return this.getStage(1);
-            if(input == 3) return getButton(3);
+            if(input == 3) return this.getButton(3);
             if(input == 4) return 4;
             break; 
         // stage 4
         case 4:
             if(input == 1) return this.getStage(1);
-            if(input == 2) return getButton(1);
+            if(input == 2) return this.getButton(1);
             if(input == 3) return this.getStage(2);
             if(input == 4) return this.getStage(2);
             break;
@@ -99,6 +111,14 @@ export class MemoryComponent implements BombModuleInterface {
         }
     }
 
+    private getButton(position: number): number {
+        if(position <= 0 || position > this.buttons.length) {
+            throw "Out of index error for buttons";
+        } else {
+            return this.buttons[position-1];
+        }
+    }
+
     private getStage(stage: number): number {
         if(this.outputs.length+1 < stage) {
             throw "Cannot access stage ahead of output";
@@ -108,5 +128,9 @@ export class MemoryComponent implements BombModuleInterface {
 
     public getClass(input: number): string {
         return buttonFormatting[input];
+    }
+
+    public getDropdownClass(input: number): string {
+        return buttonDropdownFormatting[input];
     }
 }
