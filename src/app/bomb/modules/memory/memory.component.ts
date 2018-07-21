ee6d620a2eaa0d45d0291d1bad8965c2c56227bc
FiendChain <formulating.response@gmail.com>
2018-07-21 17:06:04 +1000

    Fixed logic in memory module

--- src/app/bomb/modules/memory/memory.component.ts
+++ src/app/bomb/modules/memory/memory.component.ts
@@ -23,27 +23,49 @@ const buttonDropdownFormatting: ClassFormatting = {
 })
 export class MemoryComponent implements BombModuleInterface {
     public name: string = 'memory';
+    // current button config
     public buttons: Array<number> = [1,2,3,4];
+    // the inputs for each stage
     public inputs: Array<number> = [];
+    // data for each stage
     public outputs: Array<number> = [];
+    public positions: Array<number> = [];
+    public stages: Array<Array<number>> = []; // order of buttons for each stage
+
+    public currentStage: number = 1;
     public maxStages: number = 5;
     public buttonFormatting = buttonFormatting;
 
     constructor() {}
 
+    // add input
     public addInput(input: number): void {
         if(this.inputs.length < 5) {
             this.inputs.push(input);
-            let output = this.calculateOutput(input, this.inputs.length);
-            this.outputs.push(output);
+            this.stages.push(this.buttons.slice(0)); // add to history
+            let output = this.calculateOutput(input);
+            this.addOutput(output);
+            this.currentStage++;
         }
+    }
+
+    public addOutput(output: number): void {
+        this.outputs.push(output);
+        let buttons = this.stages[this.currentStage-1];
+        buttons.forEach((value, index) => {
+            if(value == output) {
+                this.positions.push(index+1);
+            }
+        });
     }
 
     public removeIndex(index: number): void {
         this.inputs.splice(index, 1);
+        this.stages.splice(index, 1);
         this.recomputeOutput();
     }
 
+    // set value of buttons
     public setButton(index: number, newValue: number): void {
         let oldValue: number = this.buttons[index];
         // swap values
@@ -59,13 +81,16 @@ export class MemoryComponent implements BombModuleInterface {
     // if input list was changed, recompute new output sequence
     private recomputeOutput(): void {
         this.outputs = [];
+        this.positions = [];
+        this.currentStage = 1;
         this.inputs.forEach((input, index) => {
-            let output = this.calculateOutput(input, index+1);
-            this.outputs.push(output);
+            let output = this.calculateOutput(input);
+            this.addOutput(output);
+            this.currentStage++;
         });
     }
 
-    public calculateOutput(input: number, stage: number): number {
+    public calculateOutput(input: number, stage: number = this.currentStage): number {
         // validate input
         if(input <= 0 || input > 4) {
             throw "Invalid input for memory module";
@@ -81,44 +106,53 @@ export class MemoryComponent implements BombModuleInterface {
         // stage 2
         case 2:
             if(input == 1) return 4;
-            if(input == 2) return this.getStage(1);
+            if(input == 2) return this.getStagePosition(1);
             if(input == 3) return this.getButton(1);
-            if(input == 4) return this.getStage(1);
+            if(input == 4) return this.getStagePosition(1);
             break;
         // stage 3
         case 3:
-            if(input == 1) return this.getStage(2);
-            if(input == 2) return this.getStage(1);
+            if(input == 1) return this.getStageValue(2);
+            if(input == 2) return this.getStageValue(1);
             if(input == 3) return this.getButton(3);
             if(input == 4) return 4;
             break; 
         // stage 4
         case 4:
-            if(input == 1) return this.getStage(1);
+            if(input == 1) return this.getStagePosition(1);
             if(input == 2) return this.getButton(1);
-            if(input == 3) return this.getStage(2);
-            if(input == 4) return this.getStage(2);
+            if(input == 3) return this.getStagePosition(2);
+            if(input == 4) return this.getStagePosition(2);
             break;
         case 5:
-            if(input == 1) return this.getStage(1);
-            if(input == 2) return this.getStage(2);
-            if(input == 3) return this.getStage(4);
-            if(input == 4) return this.getStage(3);
+            if(input == 1) return this.getStageValue(1);
+            if(input == 2) return this.getStageValue(2);
+            if(input == 3) return this.getStageValue(4);
+            if(input == 4) return this.getStageValue(3);
             break;
         default:
             throw `There is no stage ${stage} for memory game`;
         }
     }
 
+    // get value of button from current stage
     private getButton(position: number): number {
-        if(position <= 0 || position > this.buttons.length) {
+        let buttons = this.stages[this.currentStage-1];
+        if(position <= 0 || position > buttons.length) {
             throw "Out of index error for buttons";
         } else {
-            return this.buttons[position-1];
+            return buttons[position-1];
         }
     }
 
-    private getStage(stage: number): number {
+    // get position from previous stage
+    private getStagePosition(position: number): number {
+        let index: number = this.positions[position-1];
+        return this.getButton(index);
+    }
+
+    // get value of button from a stage
+    private getStageValue(stage: number): number {
         if(this.outputs.length+1 < stage) {
             throw "Cannot access stage ahead of output";
         }
